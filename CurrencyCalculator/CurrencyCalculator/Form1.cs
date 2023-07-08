ada791ff2ea219ad2469cb01e88f007cafb2eced
Illia <ilmo2004321@gmail.com>
2023-07-08 21:21:31 +0300

    New gui

--- CurrencyCalculator/CurrencyCalculator/Form1.cs
+++ CurrencyCalculator/CurrencyCalculator/Form1.cs
@@ -26,5 +26,20 @@ namespace CurrencyCalculator
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
